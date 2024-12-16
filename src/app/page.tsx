a0e53024c26b4cd923ef0793c4d0579b696e8ea0
Shehab Ahmed Hatem <it.22.15@esraa.edu.iq>
2024-12-16 18:14:52 +0300

    last
    last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import StatsGrid from '@/components/Stats/StatsGrid';
 export default function Home() {
   return (
     <main className="container mx-auto p-4 max-w-7xl">
-      <header className="text-center mb-8">
+      {/* <header className="text-center mb-8">
         <h1 className="text-3xl font-bold text-gray-900 mb-2">نظام حجز الفيلا</h1>
         <p className="text-gray-600">نظام إدارة الحجوزات والمدفوعات</p>
-      </header>
+      </header> */}
 
       <Calendar />
       <StatsGrid />
