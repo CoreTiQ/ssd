69b9d648d4b9738ecaf3cb223294acc5921e2aaa
Shehab Ahmed Hatem <it.22.15@esraa.edu.iq>
2024-12-17 12:52:01 +0300

    ff
    ff

---
+++ src/components/ExpensesSystem/index.tsx
@@ -0,0 +1,9 @@
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1
+  };
+  
+  export const metadata = {
+    title: 'نظام حجز الفيلا',
+    description: 'نظام إدارة حجوزات وإيجارات الفيلا اليومية',
+  };
